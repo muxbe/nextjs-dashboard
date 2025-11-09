76ece9fb6a23c80b62b2b27d7f2f52ded3d00018
muxbe <159569051+muxbe@users.noreply.github.com>
2025-11-09 13:34:41 +0100

    feat: Complete all necessary fixes for Vercel deployment and database access

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
        // Add your Next.js configuration options here if you have any
        // For example:
        // reactStrictMode: true,
+        experimental: {
+        turbopack: {
+          root: 'C:/Users/MSI/Desktop/next project/nextjs-dashboard', // Use forward slashes
+        },
+      },
      };
 
      module.exports = nextConfig;
